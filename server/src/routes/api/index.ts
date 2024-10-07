446ce15b3d10103141f73275f46dcc793ad2faf2
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-06 22:29:05 -0400

    added authenticateToken middleware for just ticket routes; allows testing api to create new users, etc.

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { ticketRouter } from './ticket-routes.js';
 import { userRouter } from './user-routes.js';
+import { authenticateToken } from '../../middleware/auth.js';
 
 const router = Router();
 
-router.use('/tickets', ticketRouter);
+// authenticate token only for ticket routes; so we can test api and add users
+router.use('/tickets', authenticateToken, ticketRouter);
 router.use('/users', userRouter);
 
 export default router;
