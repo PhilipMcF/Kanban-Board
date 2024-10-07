e0c68920875468302b87d85a5b38e07244bfa38d
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-06 22:28:32 -0400

    removed middleware import; so we can actually test api

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
-import { authenticateToken } from '../middleware/auth.js';
+// import { authenticateToken } from '../middleware/auth.js';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-// Add authentication to the API routes
-router.use('/api', authenticateToken, apiRoutes);
+// Removed authentication to all API routes
+// only for ticket routes so we can add users for testing api and actually login without seeding DB with preset users
+router.use('/api', apiRoutes);
 
 export default router;
